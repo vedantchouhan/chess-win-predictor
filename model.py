d83ed0a7840313925c37f3c7f581e7605a3394ca
vedantchouhan <chouhanvedant84@gmail.com>
2026-06-08 14:32:13 +0530

    79% final accuracy

--- model.py
+++ model.py
@@ -35,7 +35,13 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Train
-model = RandomForestClassifier(n_estimators=200, random_state=42)
+model = RandomForestClassifier(
+    n_estimators=200,
+    max_depth=10,
+    min_samples_split=10,
+    min_samples_leaf=2,
+    random_state=42
+)
 model.fit(X_train, y_train)
 
 # Evaluate
